d15f9164190f453114891cdd4a779e9122307c8e
shukhratov umar <ushukhratov@gmail.com>
2023-06-14 17:23:55 +0500

    Init

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,24 @@
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
+export default function Footer() {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: 150,
+        bgcolor: "inherit",
+        px: "60px",
+      }}
+    >
+    </Box>
+  );
+}
